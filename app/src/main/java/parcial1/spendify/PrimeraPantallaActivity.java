6d8393a442f8a0051e9498672040ad1e2e851154
Antonella Villani <115169494+antonellavillani@users.noreply.github.com>
2023-11-25 19:39:36 -0300

    feat: Añadí evento OnClickListener al botón "Comenzar" en PrimeraPantallaActivity.java

--- app/src/main/java/parcial1/spendify/PrimeraPantallaActivity.java
+++ app/src/main/java/parcial1/spendify/PrimeraPantallaActivity.java
@@ -2,7 +2,10 @@ package parcial1.spendify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PrimeraPantallaActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class PrimeraPantallaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_primera_pantalla);
+
+        // Evento --> botón comenzar lleva a la pantalla de crear una cuenta
+        Button botonComenzar = findViewById(R.id.boton_comenzar);
+
+        botonComenzar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crear una intención para iniciar la actividad MainActivity
+                Intent intent = new Intent(PrimeraPantallaActivity.this, MainActivity.class);
+
+                // Iniciar la actividad MainActivity
+                startActivity(intent);
+            }
+        });
     }
 }
