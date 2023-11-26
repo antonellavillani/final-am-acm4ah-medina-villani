d3acb44d88f53504136ad6d34b5ce20297755d08
Antonella Villani <115169494+antonellavillani@users.noreply.github.com>
2023-11-26 18:26:15 -0300

    feat: Modifiqué el archivo activity_pantalla_ingresar_sueldo.xml: cambio de botones de opciones por un RadioGroup, añadí un botón para continuar, archivos styles.xml, selector_radiobutton.xml boton_pulsado.xml y boton_normal.xml y nuevas variables en strings.xml y dimensions.xml

--- app/src/main/java/parcial1/spendify/PantallaIngresarSueldo.java
+++ app/src/main/java/parcial1/spendify/PantallaIngresarSueldo.java
@@ -1,8 +1,10 @@
 package parcial1.spendify;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class PantallaIngresarSueldo extends AppCompatActivity {
 
@@ -10,5 +12,24 @@ public class PantallaIngresarSueldo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_ingresar_sueldo);
+
+        // Obtener referencia al RadioGroup
+        RadioGroup radioGroupOpciones = findViewById(R.id.radioGroupOpciones);
+
+        // Configuración del listener para los cambios en el RadioGroup
+        radioGroupOpciones.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // Desmarcar todos los botones
+                for (int i = 0; i < group.getChildCount(); i++) {
+                    RadioButton radioButton = (RadioButton) group.getChildAt(i);
+                    radioButton.setChecked(false);
+                }
+
+                // Marcar el botón seleccionado
+                RadioButton selectedRadioButton = findViewById(checkedId);
+                selectedRadioButton.setChecked(true);
+            }
+        });
     }
 }
