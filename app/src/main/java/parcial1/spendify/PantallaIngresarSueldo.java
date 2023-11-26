cce264949e1159f9f7c19949e884bd2b4bf4d0a0
Antonella Villani <115169494+antonellavillani@users.noreply.github.com>
2023-11-26 18:53:44 -0300

    feat: Agregué la lógica para habilitar/deshabilitar botón "Siguiente" en PantallaIngresarSueldo.java

--- app/src/main/java/parcial1/spendify/PantallaIngresarSueldo.java
+++ app/src/main/java/parcial1/spendify/PantallaIngresarSueldo.java
@@ -1,22 +1,29 @@
 package parcial1.spendify;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PantallaIngresarSueldo extends AppCompatActivity {
+
+    private Button botonSiguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_ingresar_sueldo);
 
-        // Obtener referencia al RadioGroup
         RadioGroup radioGroupOpciones = findViewById(R.id.radioGroupOpciones);
+        botonSiguiente = findViewById(R.id.boton_siguiente);
 
-        // Configuración del listener para los cambios en el RadioGroup
+        // Deshabilitar el botón "Siguiente" por defecto
+        botonSiguiente.setEnabled(false);
+
+        // Configurar el listener para los cambios en el RadioGroup
         radioGroupOpciones.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
@@ -29,6 +36,21 @@ public class PantallaIngresarSueldo extends AppCompatActivity {
                 // Marcar el botón seleccionado
                 RadioButton selectedRadioButton = findViewById(checkedId);
                 selectedRadioButton.setChecked(true);
+
+                // Habilitar el botón "Siguiente"
+                botonSiguiente.setEnabled(true);
+            }
+        });
+
+        // Configurar el listener para el botón "Siguiente"
+        botonSiguiente.setOnClickListener(v -> {
+            // Verificar si se ha seleccionado alguna opción
+            if (radioGroupOpciones.getCheckedRadioButtonId() == -1) {
+                // Mostrar Toast si no se ha seleccionado ninguna opción
+                Toast.makeText(PantallaIngresarSueldo.this, "Selecciona una opción", Toast.LENGTH_SHORT).show();
+            } else {
+                // Guardar la opción seleccionada para usar después
+                int opcionSeleccionada = radioGroupOpciones.getCheckedRadioButtonId();
             }
         });
     }
