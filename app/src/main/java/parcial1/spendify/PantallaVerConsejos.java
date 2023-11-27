02d8ae55396adfa522b6ede32853dfc1f2365027
Antonella Villani <115169494+antonellavillani@users.noreply.github.com>
2023-11-27 18:04:55 -0300

    feat: Agregué PantallaVerConsejos.java

---
+++ app/src/main/java/parcial1/spendify/PantallaVerConsejos.java
@@ -0,0 +1,32 @@
+package parcial1.spendify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PantallaVerConsejos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_ver_consejos);
+
+        // OnClickListener para el botón "Volver"
+        Button botonVolver = findViewById(R.id.boton_volver);
+        botonVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                volverAPantallaIndex();
+            }
+        });
+    }
+
+    private void volverAPantallaIndex() {
+        Intent intent = new Intent(this, PantallaIndex.class);
+        startActivity(intent);
+        finish(); // Cierra la actividad actual para que no quede en la pila de actividades
+    }
+}
