198af0b85823ffb6b1d0c85ff6cbc99231e26e64
Antonella Villani <115169494+antonellavillani@users.noreply.github.com>
2023-11-27 16:18:26 -0300

    feat: Agregué la PantallaVerGastos

--- app/src/main/java/parcial1/spendify/PantallaIndex.java
+++ app/src/main/java/parcial1/spendify/PantallaIndex.java
@@ -2,7 +2,10 @@ package parcial1.spendify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PantallaIndex extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class PantallaIndex extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_index);
+
+        // ------------ OnClickListener para botones ------------
+
+        // Botón 'Gastos'
+        Button botonGastos = findViewById(R.id.boton_gastos);
+        botonGastos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirPantallaVerGastos();
+            }
+        });
+
+
     }
+
+        // ------------ Funciones para abrir pantallas ------------
+
+        // PantallaVerGastos
+        private void abrirPantallaVerGastos() {
+            Intent intent = new Intent(this, PantallaVerGastos.class);
+            startActivity(intent);
+        }
 }
