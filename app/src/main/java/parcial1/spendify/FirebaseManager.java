33fcd1fc6a10da20ba20c1e96393b912dfff2db8
Antonella Villani <115169494+antonellavillani@users.noreply.github.com>
2023-11-27 23:53:45 -0300

    fix: Modifiqué la lógica de PantallaGastosFijos.java implementando FirebaseManager.java

--- app/src/main/java/parcial1/spendify/FirebaseManager.java
+++ app/src/main/java/parcial1/spendify/FirebaseManager.java
@@ -1,8 +1,14 @@
 package parcial1.spendify;
 
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FirebaseManager {
     private static FirebaseManager instance;
@@ -43,6 +49,39 @@ public class FirebaseManager {
 
     public FirebaseUser getCurrentUser() {
         return mAuth.getCurrentUser();
+    }
+
+    public void agregarGasto(String userEmail, String tipoGasto, String monto) {
+        // Obtener referencia a la colección de gastos para el usuario
+        CollectionReference gastosCollection = firestore.collection("usuarios").document(userEmail).collection("gastos");
+
+        // Crear un mapa con los datos del gasto
+        Map<String, Object> gasto = new HashMap<>();
+        gasto.put("monto", monto);
+
+        // Agregar el documento a la colección de gastos con el tipo de gasto como ID
+        gastosCollection.document(tipoGasto).set(gasto);
+    }
+
+    public void eliminarGasto(String userEmail, String tipoGasto) {
+        // Obtener referencia a la colección de gastos para el usuario
+        CollectionReference gastosCollection = firestore.collection("usuarios").document(userEmail).collection("gastos");
+
+        // Eliminar el documento correspondiente al tipo de gasto
+        gastosCollection.document(tipoGasto).delete();
+    }
+
+    public Task<Void> actualizarGasto(String userEmail, String tipoGastoActual, String nuevoTipoGasto, String nuevoMonto) {
+        // Obtener referencia al documento del usuario y tipo de gasto actual
+        DocumentReference gastoRef = firestore.collection("usuarios").document(userEmail).collection("gastos").document(tipoGastoActual);
+
+        // Crear un mapa con los nuevos datos a actualizar
+        Map<String, Object> nuevosDatos = new HashMap<>();
+        nuevosDatos.put("tipoGasto", nuevoTipoGasto);
+        nuevosDatos.put("monto", nuevoMonto);
+
+        // Actualizar los datos en Firestore
+        return gastoRef.update(nuevosDatos);
     }
 
     public interface AuthCallback {
--- app/src/main/java/parcial1/spendify/PantallaGastosFijos.java
+++ app/src/main/java/parcial1/spendify/PantallaGastosFijos.java
@@ -1,33 +1,51 @@
 package parcial1.spendify;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseUser;
 
 public class PantallaGastosFijos extends AppCompatActivity {
 
     private TableLayout tableLayout;
+    private EditText editTextTipoGasto;
+    private EditText editTextMonto;
+
+    private FirebaseManager firebaseManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_gastos_fijos);
 
-        // Obtener referencia a la tabla
+        // Obtener referencia a la tabla y a los EditText
         tableLayout = findViewById(R.id.tableLayout);
+        editTextTipoGasto = findViewById(R.id.EditText_ingresar_tipo_de_gasto);
+        editTextMonto = findViewById(R.id.EditText_ingresar_monto);
+
+        // Configurar FirebaseManager
+        firebaseManager = FirebaseManager.getInstance();
 
         // Ejemplo de agregar filas a la tabla
         agregarFilaTabla("Alquiler", "$1000");
         agregarFilaTabla("Electricidad", "$150");
     }
 
-    // Función para agregar una fila a la tabla
     private void agregarFilaTabla(String tipoGasto, String monto) {
         // Crear una nueva fila
         TableRow newRow = new TableRow(this);
@@ -51,13 +69,85 @@ public class PantallaGastosFijos extends AppCompatActivity {
 
         Button cellModificar = new Button(this);
         cellModificar.setText("Modificar");
+
         // Configurar onClickListener para el botón "Modificar"
         cellModificar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Lógica para modificar
+                // Obtener el tipo de gasto actual y el monto actual
+                String tipoGastoActual = cellTipoGasto.getText().toString();
+                String montoActual = cellMonto.getText().toString();
+
+                // Crear un cuadro de diálogo
+                AlertDialog.Builder builder = new AlertDialog.Builder(PantallaGastosFijos.this);
+                builder.setTitle("Editar Datos");
+
+                // Inflar el diseño del cuadro de diálogo
+                View view = getLayoutInflater().inflate(R.layout.dialog_editar_gasto, null);
+                builder.setView(view);
+
+                // Obtener referencias a los EditText dentro del cuadro de diálogo
+                EditText editTextTipoGasto = view.findViewById(R.id.editText_tipo_gasto);
+                EditText editTextMonto = view.findViewById(R.id.editText_monto);
+
+                // Establecer los valores actuales en los EditText
+                editTextTipoGasto.setText(tipoGastoActual);
+                editTextMonto.setText(montoActual);
+
+                // Configurar los botones del cuadro de diálogo
+                builder.setPositiveButton("Guardar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Obtener los nuevos valores desde los EditText
+                        String nuevoTipoGasto = editTextTipoGasto.getText().toString();
+                        String nuevoMonto = editTextMonto.getText().toString();
+
+                        // Obtener el tipo de gasto actual y el monto actual
+                        String tipoGastoActual = cellTipoGasto.getText().toString();
+                        String montoActual = cellMonto.getText().toString();
+
+                        // Obtener el usuario actual
+                        FirebaseUser currentUser = firebaseManager.getCurrentUser();
+
+                        if (currentUser != null) {
+                            // Obtener el correo electrónico del usuario
+                            String userEmail = currentUser.getEmail();
+
+                            // Actualizar el gasto en Firestore
+                            firebaseManager.actualizarGasto(userEmail, tipoGastoActual, nuevoTipoGasto, nuevoMonto)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                // Actualizar las celdas en la fila
+                                                cellTipoGasto.setText(nuevoTipoGasto);
+                                                cellMonto.setText(nuevoMonto);
+                                                Toast.makeText(PantallaGastosFijos.this, "Datos actualizados correctamente", Toast.LENGTH_SHORT).show();
+                                            } else {
+                                                // Manejar errores si la actualización no es exitosa
+                                                Toast.makeText(PantallaGastosFijos.this, "Error al actualizar datos", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
+                        } else {
+                            // El usuario no está autenticado
+                            Toast.makeText(PantallaGastosFijos.this, "Error: Usuario no autenticado", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+                builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Cerrar el cuadro de diálogo sin hacer cambios
+                    }
+                });
+
+                // Mostrar el cuadro de diálogo
+                builder.create().show();
             }
         });
+
 
         Button cellEliminar = new Button(this);
         cellEliminar.setText("Eliminar");
@@ -66,6 +156,9 @@ public class PantallaGastosFijos extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Lógica para eliminar
+                eliminarGasto(tipoGasto);
+                // Remover la fila de la tabla
+                tableLayout.removeView(newRow);
             }
         });
 
@@ -81,5 +174,63 @@ public class PantallaGastosFijos extends AppCompatActivity {
         // Cambiar visibilidad de la tabla a VISIBLE después de agregar una fila
         tableLayout.setVisibility(View.VISIBLE);
     }
-}
 
+    // Función para eliminar un gasto de Firestore
+    private void eliminarGasto(String tipoGasto) {
+        // Obtener el usuario actual
+        FirebaseUser currentUser = firebaseManager.getCurrentUser();
+
+        if (currentUser != null) {
+            // Obtener el correo electrónico del usuario
+            String userEmail = currentUser.getEmail();
+
+            // Eliminar el gasto de Firestore
+            firebaseManager.eliminarGasto(userEmail, tipoGasto);
+        } else {
+            // El usuario no está autenticado
+            Toast.makeText(this, "Error: Usuario no autenticado", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void agregarGasto(View view) {
+        String tipoGasto = editTextTipoGasto.getText().toString();
+        String monto = editTextMonto.getText().toString();
+
+        // Verificar que ambos campos estén completos
+        if (!tipoGasto.isEmpty() && !monto.isEmpty()) {
+            // Agregar datos a Firestore
+            agregarGastoFirestore(tipoGasto, monto);
+            // Agregar fila a la tabla
+            agregarFilaTabla(tipoGasto, monto);
+            // Limpiar campos después de agregar
+            editTextTipoGasto.getText().clear();
+            editTextMonto.getText().clear();
+        } else {
+            // Mostrar mensaje si los campos están vacíos
+            Toast.makeText(this, "Ingresa todos los campos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void continuar(View view) {
+        // Lógica para continuar a PantallaIndex
+        Intent intent = new Intent(this, PantallaIndex.class);
+        startActivity(intent);
+    }
+
+    // Función para agregar un gasto a Firestore
+    private void agregarGastoFirestore(String tipoGasto, String monto) {
+        // Obtener el usuario actual
+        FirebaseUser currentUser = firebaseManager.getCurrentUser();
+
+        if (currentUser != null) {
+            // Obtener el correo electrónico del usuario
+            String userEmail = currentUser.getEmail();
+
+            // Agregar el gasto a Firestore
+            firebaseManager.agregarGasto(userEmail, tipoGasto, monto);
+        } else {
+            // El usuario no está autenticado
+            Toast.makeText(this, "Error: Usuario no autenticado", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
